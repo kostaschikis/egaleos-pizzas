a985907c192b302bf8586a7fa9402e019d5eead3
Kostas Chikimtzis <kostaschikis@gmail.com>
2020-02-24 13:26:14 +0200

    Pizza Web Form

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -35,4 +35,12 @@ class PizzaController extends Controller
     public function create() {
         return view('pizzas.create');
     }
+
+    public function store() {
+        error_log(request('name'));
+        error_log(request('type'));
+        error_log(request('base'));
+
+        return redirect('/');
+    }
 }
--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -1,11 +1,27 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height">       
-    <div class="content">
-        <div class="title m-b-md">
-            Add a new pizza
-        </div> 
-    </div>
+<div class="wrapper create-pizza">
+    <h1>Create a New Pizza</h1>
+    <form action="/pizzas" method="POST">
+        @csrf
+        <label for="name">Your name:</label>
+        <input type="text" name="name" id="name">
+        <label for="type">Choose pizza type:</label>
+        <select name="type" id="type">
+            <option value="margherita">Margherita</option>
+            <option value="hawaiian">Hawaiian</option>
+            <option value="veg supreme">Veg Supreme</option>
+            <option value="volcano">Volcano</option>
+        </select>
+        <label for="base">Choose base type:</label>
+        <select name="base" id="base">
+            <option value="cheesy crust">Cheesy Crust</option>
+            <option value="garlic crust">Garlic Crust</option>
+            <option value="thin & cripsy">Thin & Crispy</option>
+            <option value="thick">Thick</option>
+        </select>
+        <input type="submit" value="Order Pizza">
+    </form>
 </div>
 @endsection
