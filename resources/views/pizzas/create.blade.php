2808f0bca5e7de9d71f19f6006a57d7c2925475d
Kostas Chikimtzis <kostaschikis@gmail.com>
2020-03-01 17:07:55 +0200

    modified pizzas table, toppings JSON

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -21,6 +21,14 @@
             <option value="thin & cripsy">Thin & Crispy</option>
             <option value="thick">Thick</option>
         </select>
+        <fieldset>
+            <label for="">Extra toppings</label> <br>
+            <input type="checkbox" name="toppings[]" value="mushrooms">Mushrooms<br>
+            <input type="checkbox" name="toppings[]" value="peppers">Peppers<br>
+            <input type="checkbox" name="toppings[]" value="garlic">Garlic<br>
+            <input type="checkbox" name="toppings[]" value="olives">Olives<br>
+
+        </fieldset>
         <input type="submit" value="Order Pizza">
     </form>
 </div>
